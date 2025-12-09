57874fbbe87e4b6224f3774aa1b987c2ae72fa2a
Lu <elgranchipiron@outlook.es>
2025-12-09 17:46:56 +0100

    cors importado y funcionando

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,14 @@
 import db from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-db.connect(process.env.DB).then(() => {
+db.connect(process.env.DB)
+  .then(() => {
     console.log("Connected to MongoDB");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 export default db;
-
-
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from "express";
-import dotenv from "dotenv";
 import Router from "./routes/sign.routes.js";
 import verifyRouter from "./routes/verify.route.js";
 import { authMiddleware } from "./middleware/auth.middleware.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Wiwiwiwiwiii");
